7c527bdd647a205ab4d8074a8a5fef1895aa88d7
Samantha Emily-Rachel Belnavis <me@samantharachelb.ca>
2018-07-14 04:06:17 -0400

    Initial Commit

---
+++ colourvision/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  colourvision
+//
+//  Created by Samantha Emily-Rachel Belnavis on 2018-07-14.
+//  Copyright © 2018 Samantha Emily-Rachel Belnavis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
