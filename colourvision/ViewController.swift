8a191f663bdde0f4c2672fe894de6b0ad81316b1
Samantha Emily-Rachel Belnavis <me@samantharachelb.ca>
2018-07-14 04:20:36 -0400

    Platform change

--- colourvision/ViewController.swift
+++ colourvision/ViewController.swift
@@ -6,20 +6,13 @@
 //  Copyright © 2018 Samantha Emily-Rachel Belnavis. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
